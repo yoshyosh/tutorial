ee126584473172236f7028c541e3576de4a5d690
Joseph Anderson <janderson760@gmail.com>
2011-05-23 01:34:39 -0700

    Can create lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,15 +1,47 @@
 class LessonsController < ApplicationController
 
 	def index
+		@lessons = Lesson.all
+		
+		respond_to do |format|
+		format.html # index.html.erb
+		format.xml { render :xml => @lessons }
+		end
 	end
 	
 	def new
+		@lesson = Lesson.new
+		
+		respond_to do |format|
+		format.html
+		format.xml { render :xml => @lesson }
+		end
 	end
 	
 	def create
+		@lesson = Lesson.new(params[:lesson])
+			
+			respond_to do |format|
+			if @lesson.save
+				format.html { redirect_to(@lesson,
+										:notice => 'Lesson was successfully created!') }
+				format.xml { render :xml => @lesson,
+										:status => :created, :location => @post }
+			else
+				format.html { render :action => "new" }
+				format.xml { render :xml => @post.errors,
+										:status => :unprocessable_entity }
+			end
+		end
 	end
 	
 	def show
+		@lesson = Lesson.find(params[:id])
+		
+		respond_to do |format|
+			format.html
+			format.xml { render :xml => @lesson }
+		end
 	end
 	
 	def edit
--- db/migrate/20110523070605_create_lessons.rb
+++ db/migrate/20110523070605_create_lessons.rb
@@ -1,6 +1,9 @@
 class CreateLessons < ActiveRecord::Migration
   def self.up
     create_table :lessons do |t|
+	  t.string :subject
+	  t.string :image
+	  t.text :description
 
       t.timestamps
     end
