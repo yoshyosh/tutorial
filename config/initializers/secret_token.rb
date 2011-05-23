35acea1f4cb5a2ab7dae5adeb85064eac069d2d4
Joseph Anderson <janderson760@gmail.com>
2011-05-23 00:13:47 -0700

    Start tutorial page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial::Application.config.secret_token = '2d83e1ed79da6b3222a119fbde94c1d63490804bc097750ff36b94cc85bdee44ba85837987760f34a1e1de2c7df2fa86dbb33661a9c3a9c9688bf3d870c62505'
